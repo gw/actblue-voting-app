aa3949230f584e4cbf8cc715940e4b718412f693
gw <4099071+gw@users.noreply.github.com>
2025-08-20 20:54:26 -0700

    Remove extraneous Candidate.vote_count method

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,9 +1,5 @@
 class Candidate < ApplicationRecord
   has_many :voters, class_name: 'User', foreign_key: 'voted_for_candidate_id', dependent: :nullify
-  
+
   validates :name, presence: true, uniqueness: true
-  
-  def vote_count
-    voters.count
-  end
 end
